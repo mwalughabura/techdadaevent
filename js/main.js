2c4294db3c274e3539b022381e75ae4b1ab20448
mwalughabura <mwalughabura@gmail.com>
2017-07-02 10:50:33 +0300

    almost done

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
-function loadMap() { 
-   var mapOptions = { 
-   center:new google.maps.LatLng(17.609993, 83.221436), 
-   zoom:6 
-   mapTypeId:google.maps.MapTypeId.ROADMAP 
-  }; 
-  var map=new google.maps.Map(document.getElementById("sample"),mapOptions); 
-} 
-google.maps.event.addDomListener(window, 'load', loadMap); 
+$(document).ready(function(){
+        // set up hover panels
+        // although this can be done without JavaScript, we've attached these events
+        // because it causes the hover to be triggered when the element is tapped on a touch device
+        $('.hover').hover(function(){
+            $(this).addClass('flip');
+        },function(){
+            $(this).removeClass('flip');
+        });
+    });
